e4e3db52496edd62a075fb76f60412a5e6ceddd0
murilokrugner <murilokrugner@hotmail.com>
2020-03-30 18:13:26 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,44 @@
+// importando o express
+import express from "express";
+
+// importando o path
+import path from "path";
+
+// importando o cors
+import cors from "cors";
+
+// importando arquivo de rotas
+import routes from "./routes";
+
+import "./database";
+
+// criando uma classe para configuração do servidor
+class App {
+  constructor() {
+    // atribuindo o express a var server
+    this.server = express();
+
+    // chamando os middlewares
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    // configuração para enviar json
+    this.server.use(express.json());
+    // retornar arquivos estaticos
+    this.server.use(
+      "/files",
+      express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+    );
+    this.server.use(cors());
+  }
+
+  routes() {
+    // configuração das rotas pelo arquivos routes.js
+    this.server.use(routes);
+  }
+}
+
+// exportando o server
+export default new App().server;
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "geomaps",
+  database: "postgres",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
